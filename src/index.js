032485e3169d6256284dfe4d85a1074e27152320
Levi Winans <levieiko@gmail.com>
2019-10-14 08:36:02 -0700

    set up firebase and dependencies, Hello World

--- src/index.js
+++ src/index.js
@@ -3,10 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/storage';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  firebase.initializeApp({
+    apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    authDomain: "prana-wellness.firebaseapp.com",
+    databaseURL: "https://prana-wellness.firebaseio.com",
+    projectId: "prana-wellness",
+    storageBucket: "prana-wellness.appspot.com",
+    messagingSenderId: "924601017843",
+    appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
+  });
+  const storage = firebase.storage();
+  export { storage, firebase as default }
+
+
+  ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root'));
+
 serviceWorker.unregister();
+
+
