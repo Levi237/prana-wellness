b8951cecc91d67dcce1471bf0171b3092b10627c
Levi Winans <levieiko@gmail.com>
2019-10-14 15:26:32 -0700

    center box, align image behind

--- src/components/PointDisplay.js
+++ src/components/PointDisplay.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
 
+import './PointDisplay.css'
+
 export default class PointDisplay extends Component {
     state = {
         points: [{ 
             smallText: "Corporate",
             largeText: "Wellness",
+            image: "point-placeholder.png"
         },{
             smallText: "Invigorating",
             largeText: "Yoga",
+            image: "point-placeholder.png"
         },{
             smallText: "relaxing",
             largeText: "Meditation",
+            image: "point-placeholder.png"
         },{
             smallText: "Mindful",
             largeText: "Breathing",
+            image: "point-placeholder.png"
         }]
         }
     render(){
         const { points } = this.state
         console.log(points);
-        const showPoints = points.map(point => {
+        const showPoints = points.map((point, key) => {
             return(
-                <div>{point.smallText}</div>
+                <div key={key} className="point-box">
+                    <div>
+                        <section>{point.smallText}</section><section>{point.largeText}</section>
+                    </div>
+                    <img className="pointBackgroundImage" src="point-placeholder.png" alt="point-box" />
+                </div>
             )
         })
         return(<>{showPoints}</>)
