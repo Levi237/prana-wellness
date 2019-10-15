e2e126ade76ff0d6bb4d384073b89dc583520558
Levi Winans <levieiko@gmail.com>
2019-10-15 09:36:59 -0700

    add logo to home header

--- src/App.js
+++ src/App.js
@@ -18,7 +18,9 @@ export default class App extends Component {
         </div>
         <div className="grid-nav">
           <div className="home-logo">
-            LOGO
+          <div className="logo-box">
+            <img src="icon_white.png" alt="icon-white" />
+          </div>
           </div>
           <div className="home-hamburger">
             <section></section><section></section><section></section>
