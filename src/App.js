7ee76d3e8900e9d1692a4e595014566e174d2233
Levi Winans <levieiko@gmail.com>
2019-10-15 09:39:18 -0700

    save footer

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ export default class App extends Component {
         </div>
         <div className="grid-nav">
           <div className="home-logo">
-          <div className="logo-box">
-            <img src="icon_white.png" alt="icon-white" />
-          </div>
+            <div className="logo-box">
+              <img src="icon_white.png" alt="icon-white" />
+            </div>
           </div>
           <div className="home-hamburger">
             <section></section><section></section><section></section>
