3d9dba011ccbf7915f95ddbc136aed7f61909ad8
Levi Winans <levieiko@gmail.com>
2019-10-15 09:48:45 -0700

    add nav

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React       from 'react';
+import { NavLink } from 'react-router-dom';
+
+import * as routes from '../../constants/routes'
+
+const Nav = () => 
+    <div className="nav-container">
+
+        <NavLink activeClassName="nav-active" to={routes.HOME}>HOME</NavLink>
+        <NavLink activeClassName="nav-active" to={routes.INFO}>ABOUT</NavLink>
+
+    </div>
+
+export default Nav
