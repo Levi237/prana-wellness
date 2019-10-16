b8543ba3443b0044da892ba8a5bc9bf612aabe87
Levi Winans <levieiko@gmail.com>
2019-10-16 10:49:15 -0700

    content aligned in header image

--- src/components/About/AboutHeader.js
+++ src/components/About/AboutHeader.js
@@ -2,11 +2,15 @@ import React from 'react';
 
  const AboutHeader = () => {
     return(<>
-        <div className="header-content-container">
-            <section className="top-header-text pranaText">THE TEAM BEHIND</section>
-            <section className="bottom-header-text">Prana Wellness</section>
-        </div>
-        <img className="header-image" src="about-header.png" alt="team image"/>
+                <div className="header-container">
+                    <img className="header-image" src="about-header.png" alt="team image"/>
+                    <div className="header-content-container">
+                        <section className="top-header-text pranaText">THE TEAM BEHIND</section>
+                        <section className="bottom-header-text">Prana Wellness</section>
+                    </div>
+                </div>
+                {/* <div className="wellness-header-purple"></div> */}
+    
             </>)
 }
 
