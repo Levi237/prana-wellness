b0d46076700d8336245d45b09ab84597e5b32ac0
Levi Winans <levieiko@gmail.com>
2019-10-16 16:18:07 -0700

    clean up code

--- src/components/About/AboutHeader.js
+++ src/components/About/AboutHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
  const AboutHeader = () => {
     return(<>
                 <div className="header-container">
-                    <img className="header-image" src="about-header.png" alt="team image"/>
+                    <img className="header-image" src="about-header.png" alt="team"/>
                     <div className="header-content-container">
                         <section className="top-header-text pranaText">THE TEAM BEHIND</section>
                         <section className="bottom-header-text">Prana Wellness</section>
