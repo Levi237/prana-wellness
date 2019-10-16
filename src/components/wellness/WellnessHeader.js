6a09c51f2faaa783c847f03272c790fe4f934158
Levi Winans <levieiko@gmail.com>
2019-10-16 09:42:25 -0700

    line up header nav logo, remove negative margins

--- src/components/wellness/WellnessHeader.js
+++ src/components/wellness/WellnessHeader.js
@@ -2,11 +2,11 @@ import React from 'react';
 
  const WellnessHeader = () => {
     return(<>
+        <img className="header-image" src="wellness-header.png" alt="wellness-header"/>
         <div className="header-content-container">
             <section className="top-header-text pranaText">REDISCOVER</section>
             <section className="bottom-header-text">Personal Wellness</section>
         </div>
-        <img className="header-image" src="wellness-header.png" alt="wellness-header"/>
         <div className="wellness-header-purple"></div>
             </>)
 }
