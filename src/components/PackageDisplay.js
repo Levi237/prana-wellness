5e9065e2684b14fa62013432c096247a3bd6a813
Levi Winans <levieiko@gmail.com>
2019-10-17 12:20:04 -0700

    move master files over add images

--- src/components/PackageDisplay.js
+++ src/components/PackageDisplay.js
@@ -5,32 +5,40 @@ import './PackageDisplay.css'
 export default class PackageDisplay extends Component {
     state = {
         pricePack: [{ 
-            title: "Corporate",
-            price: "$10/MONTH",
-            content: "blah blah blah blah blah",
-            image: "point-placeholder.png",
+            title: "Namaste",
+            price: "$300/session",
+            duration: ["1 x per month", "(6 month commitment)"],
+            details: ["Can be a mix of yoga, meditation, and/or breath work", "*See add ons for additional services"],
+            image: "namaste.png",
         },{
-            title: "Invigorating",
-            price: "$20/MONTH",
-            content: "blah blah blah blah blah",
-            image: "point-placeholder.png",
+            title: "Karma",
+            price: "$200",
+            duration: ["1 x per week", "(3 month commitment)"],
+            details: ["Access to online meditations", "1 lunch and learn ", "*See add ons for additional services"],
+            image: "karma.png",
         },{
-            title: "Mindful",
-            price: "$50/MONTH",
-            content: "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",
-            image: "point-placeholder.png",
+            title: "Guru",
+            price: "$4500",
+            duration: ["12 week", "intensive program"],
+            details: ['“Wellness for the Working Mind”', "Combination of lunch and learns, speakers, wellness classes, and team building exercises Access to various online wellness resources- yoga, stretching, breath work, meditation", "*Customizable, pricing may vary*"],
+            image: "guru.png",
         }]
     }
     render(){
         const { pricePack } = this.state
         const showPackage = pricePack.map((pack, key) => {
+            const details = pack.details.map((detail, k) => {
+                return <section>{detail}</section>
+            })
             return(
                 <div key={key} className="pack-container">
                     <img className="packBackgroundImage" src={`${pack.image}`} alt="pack-box" />
                     <div className="price-pack-box">
                         <section>{pack.title}</section>
                         <section>{pack.price}</section>
-                        <section>{pack.content}</section>
+                        <section>{pack.duration[0]}</section>
+                        <section>{pack.duration[1]}</section>
+                        {details}
                     </div>
                         <button>BUY NOW</button>
                 </div>
