4a8d94723374040ba5591f2ac0279a5ee1f7460b
Levi Winans <levieiko@gmail.com>
2019-10-17 18:41:28 -0700

    adjust naming to lowercase on images

--- src/components/PackageDisplay.js
+++ src/components/PackageDisplay.js
@@ -9,19 +9,19 @@ export default class PackageDisplay extends Component {
             price: "$300/session",
             duration: ["1 x per month", "(6 month commitment)"],
             details: ["Can be a mix of yoga, meditation, and/or breath work", "*See add ons for additional services"],
-            image: "Namaste.png",
+            image: "namaste.png",
         },{
             title: "Karma",
             price: "$200",
             duration: ["1 x per week", "(3 month commitment)"],
             details: ["Access to online meditations", "1 lunch and learn ", "*See add ons for additional services"],
-            image: "Karma.png",
+            image: "karma.png",
         },{
             title: "Guru",
             price: "$4500",
             duration: ["12 week", "intensive program"],
             details: ['“Wellness for the Working Mind”', "Combination of lunch and learns, speakers, wellness classes, and team building exercises Access to various online wellness resources- yoga, stretching, breath work, meditation", "*Customizable, pricing may vary*"],
-            image: "Guru.png",
+            image: "guru.png",
         }]
     }
     render(){
