1fd596eb8f46831cb6cd0b2edda46cbcd80e6386
Levi Winans <levieiko@gmail.com>
2019-10-17 17:45:14 -0700

    save nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,15 +10,10 @@ const divStyle = {
 
 export default class Nav extends Component {
 
-    // onClose = (e) => {
-    //     this.props.onClose && this.props.onClose(e);
-    // }
 
     render(){
         const { toggleHamburger } = this.props
-        // if (!this.props.showNav){
-        //     return null;
-        // }
+
         
         return(
             <div id="menu" className="nav-container inactive" style={divStyle}>
