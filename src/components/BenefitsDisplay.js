3fd576be0d112b6dd96c0ec2dee72083b2d1df6e
Levi Winans <levieiko@gmail.com>
2019-10-18 13:18:29 -0700

    set up basic BeneifitsDisplay Component

--- src/components/BenefitsDisplay.js
+++ src/components/BenefitsDisplay.js
@@ -2,19 +2,13 @@ import React, { Component } from 'react';
 
 import './BenefitsDisplay.css'
 
-export default class PointDisplay extends Component {
-    state = {
+export default class BenefitsDisplay extends Component {
 
-    }
     render(){
-
-            return(
-                <div className="benefits-box">
-                    <section>benefits of wellness in the workplace</section>
-
-                </div>
-            )
-
-
+        return(
+            <div className="benefits-box">
+                <section>benefits of wellness in the workplace</section>
+            </div>
+        )
     }
 }
