3953620e58dafd18ec8981b5a32758b5191f7399
Levi Winans <levieiko@gmail.com>
2019-10-22 09:56:40 -0700

    mobile services display

--- src/components/services/ServicesMain.js
+++ src/components/services/ServicesMain.js
@@ -17,13 +17,42 @@ export default class ServicesMain extends Component {
             smallText: "Mindful",
             largeText: "Breathing",
             image: "breathing-icon.png"
+        }],
+        otherServices: [{ 
+            smallText: "Invigorating",
+            largeText: "Yoga",
+            image: "yoga-icon.png"
+        },{
+            smallText: "Relaxing",
+            largeText: "Meditation",
+            image: "meditation-icon.png"
+        },{
+            smallText: "Mindful",
+            largeText: "Breathing",
+            image: "breathing-icon.png"
+        },{
+            smallText: "Invigorating",
+            largeText: "Yoga",
+            image: "yoga-icon.png"
+        },{
+            smallText: "Relaxing",
+            largeText: "Meditation",
+            image: "meditation-icon.png"
+        },{
+            smallText: "Mindful",
+            largeText: "Breathing",
+            image: "breathing-icon.png"
         }]
-    }
+      }
     render(){
     return(<>
           <div className="wellness-main-container main-title">
             <ServiceDisplay services={this.state.services}/>
+            <button>Corporate Solutions</button> <button>Individual Solutions</button>
           </div>
+          <section className="home-main-header">WELLNESS SERVICES</section>
+          <ServiceDisplay services={this.state.otherServices}/>
+          <button>Request Quote</button>
     </>)
 }
 }
