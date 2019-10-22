1e8829c241712987842e5c2974ace6838baa92af
Levi Winans <levieiko@gmail.com>
2019-10-22 08:35:55 -0700

    size services containers, align main under header to scale

--- src/components/services/ServicesHeader.js
+++ src/components/services/ServicesHeader.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import ServiceDisplay from './ServiceDisplay';
 
-const featureDivStyle = {
-        height: '30px',
-        position: 'relative',
-        'z-index': '0',
-        'margin-top': '-150px',
-        height: 'calc(15vw + 200px)',
-        'text-align': 'center',
-        'background-color': 'rgb(191, 158, 200)',
-        'padding-top': '200px',
-}
+// import ServiceDisplay from './ServiceDisplay';
+
+
+// const featureDivStyle = {
+//         height: '30px',
+//         position: 'relative',
+//         'z-index': '0',
+//         'margin-top': '-150px',
+//         height: 'calc(15vw + 200px)',
+//         'text-align': 'center',
+//         'background-color': 'rgb(191, 158, 200)',
+//         'padding-top': '200px',
+//         overflow: 'auto !important',
+// }
  const ServicesHeader = () => {
     return(<>
-    <div className="header-container">
-        <img className="header-image" src="service-header.png" alt="wellness-header"/>
-        <div className="header-content-container">
-            <section className="top-header-text pranaText">explore our wide-ranging</section>
-            <section className="bottom-header-text">Wellness Services</section>
+        <div className="header-container">
+            <img className="header-image" src="service-header.png" alt="wellness-header"/>
+            <div className="header-content-container">
+                <section className="top-header-text pranaText">explore our wide-ranging</section>
+                <section className="bottom-header-text">Wellness Services</section>
+            </div>
         </div>
-    </div>
-        <div style={featureDivStyle}>
-            <ServiceDisplay />
+        <div className="wellness-header-purple">
+            {/* <ServiceDisplay /> */}
         </div>
-            </>)
+    </>)
 }
 
 export default ServicesHeader
--- src/components/services/ServicesMain.js
+++ src/components/services/ServicesMain.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-
+import ServiceDisplay from './ServiceDisplay'
 
 const ServicesMain = () => {
     return(<>
-          <div className="services-main-container">
-
+          <div className="wellness-main-container main-title">
+            <ServiceDisplay />
           </div>
     </>)
 }
