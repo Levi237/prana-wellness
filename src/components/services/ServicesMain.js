94bb9a8acd1badcde8553c2431071805322d8211
Levi Winans <levieiko@gmail.com>
2019-10-23 13:10:26 -0700

    update content state

--- src/components/services/ServicesMain.js
+++ src/components/services/ServicesMain.js
@@ -12,17 +12,17 @@ export default class ServicesMain extends Component {
             smallText: "Invigorating",
             largeText: "Yoga",
             image: "yoga-icon.png",
-            content: "All levels practice of yoga to energize the body and stretch, and focus on proper alignment ",
+            content: "All levels practice of yoga to energize the body, stretch, and focus on proper alignment and form.",
         },{
             smallText: "Relaxing",
             largeText: "Meditation",
             image: "meditation-icon.png",
-            content: "Accessible guided meditations to calm, bring focus, and de-stress",
+            content: "Accessible guided meditations to calm, bring focus, and de-stress.",
         },{
             smallText: "Mindful",
             largeText: "Breathing",
             image: "breathing-icon.png",
-            content: "Breathing practices that can have immediate impact on mind and body health",
+            content: "Breathing practices that can have immediate impact on mind and body health.",
         }],
         otherServices: [{ 
             smallText: "Bootcamp",
@@ -33,22 +33,22 @@ export default class ServicesMain extends Component {
             smallText: "Lunch +",
             largeText: "Learn",
             image: "meditation-icon.png",
-            content: "Health, fitness, and mindfulness tools and tips for balanced lifestyle and productive work.",
+            content: "Health, fitness, and mindfulness tools and tips for a balanced lifestyle and productive work experience.",
         },{
             smallText: "Corporate",
             largeText: "Massage",
             image: "breathing-icon.png",
-            content: "relaxing body break from static postures",
+            content: "Relaxing intuitive massage break from static postures.",
         },{
             smallText: "Work",
             largeText: "Play",
             image: "yoga-icon.png",
-            content: "Take time to play at work! Fun games that focus on leadership and team building",
+            content: "Take time to play at work! Fun games that focus on leadership and team building.",
         },{
             smallText: "Wellness",
             largeText: "Retreats",
             image: "retreat-other-services.png",
-            content: "Curated and customizable off-site wellness retreats for team building and training, with a fitness and wellness focus",
+            content: "Curated and customizable off-site wellness retreats for team building and training, with a fitness and wellness focus.",
         },{
             smallText: "Health",
             largeText: "Fairs",
