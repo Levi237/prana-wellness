0b38b3424126ce100459037346916fb4434c8389
Levi Winans <levieiko@gmail.com>
2019-10-23 15:46:29 -0700

    save

--- src/components/about/AboutMain.js
+++ src/components/about/AboutMain.js
@@ -5,7 +5,7 @@ import MeetStephanie  from './MeetStephanie';
 const AboutMain = () => {
     return(<>
             <section className="main-title">a holistic approach to wellness</section>
-            <section className="main-cta">“Prana is Sanskrit for breath, considered as a life-giving force. Prana is seen as a universal energy, which flows in currents in and around the body.”</section>
+            <section className="main-cta">We make wellness work for you. We educate and guide you into a happier, healthier workplace. Our wellness practices are rooted in India, at the source, and our team brings experienced wellness professionals with varying backgrounds in the corporate space. Our tailored program is designed to increase employee productivity and retention, and decrease overall healthcare costs overtime.</section>
 
           <MeetStephanie />
     </>)
