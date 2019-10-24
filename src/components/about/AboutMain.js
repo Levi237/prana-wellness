8e13af03889a716368bdf1edc8e1fabab0e985ae
Levi Winans <levieiko@gmail.com>
2019-10-23 18:43:31 -0700

    ready to deploy

--- src/components/about/AboutMain.js
+++ src/components/about/AboutMain.js
@@ -10,8 +10,8 @@ const AboutMain = () => {
               We make wellness work for you. We educate and guide you into a happier, healthier workplace. Rooted in India, at the source, our team brings experienced wellness professionals with varying backgrounds in the corporate space. Tailored programs are developed to increase employee productivity and retention, and decrease overall healthcare costs overtime. 
             </section>
             <MeetStephanie />
-            <StephanieBio />
-            <section className="main-title-two">Meet the Team</section>
+            {/* <StephanieBio /> */}
+            {/* <section className="main-title-two">Meet the Team</section> */}
     </>)
 }
 
