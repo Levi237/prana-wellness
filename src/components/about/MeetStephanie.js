4c36fc61a3443963c6287fe51d9e2a865e52e2f1
Levi Winans <levieiko@gmail.com>
2019-10-28 19:40:17 -0700

    added scroll to meet the founder

--- src/components/about/MeetStephanie.js
+++ src/components/about/MeetStephanie.js
@@ -6,15 +6,26 @@ export default class MeetStephanie extends Component {
     state = {
         meetStephanie: { 
             title: "Stepahnie Erazo",
-            subtitle: "founder, prana wellness",
-            content: "Namaste! I know the problem of work stress all too well. I spent 7 years as a Producer for one of the largest media companies in the world. After a scary panic attack at work, I decided to follow my true passion for health and wellness. 3 days after leaving my job, I journeyed to Rishikesh, India to study yoga, meditation, and breath work. From there I knew there was no turning back. I studied under master yogis, and have a true desire to spread knowledge of those traditional practices.",
+            subtitle: "Meet the Founder",
+            content: "Namaste! I know the problem of work stress all too well. I spent 7 years as a Producer for one of the largest media companies in the world. I had an exciting, rewarding, and fast-paced role there, where I got to network and mingle with many influential public figures. However, after a scary panic attack at work, I decided to follow my true passion for health and wellness. 3 days after leaving my job, I journeyed to Rishikesh, India to study yoga, meditation, and breath work. From there I knew there was no turning back. I studied under master yogis, and have a true desire to spread knowledge of those traditional practices.I am an avid adventure seeker and travel junkie, always on the move. I have been to over 25 countries, lived in Costa Rica, and Spain, and am fluent in Spanish. In my free time, you can find me cooking, boxing, hiking, traveling, playing with my dog Beamer, or trying new restaurants.",
             image: "about-meet.png",
-            skills: "Personal Trainer, Yoga Instructor + Boot Camp",
+            skills: [
+            'RYT 200, meditation + breath work',
+            'Yoga Therapy RX (candidate LMU)',
+            'Spin Instructor',
+            'Bootcamp Instructor',
+            'Personal Trainer',
+            ]
+
         }
     };
 
     render(){
         const { meetStephanie } = this.state;
+        const skillSet = meetStephanie.skills
+        const founderSkills =  skillSet.map((skill, key) => {
+            return <><span key={key}>{skill}</span><br /></>
+        })
 
             return(
                 <div className="meetStephanie-container">
@@ -23,7 +34,7 @@ export default class MeetStephanie extends Component {
                             <section>{meetStephanie.title}</section>
                             <section>{meetStephanie.subtitle}</section>
                             <section>{meetStephanie.content}</section>
-                            <section>{meetStephanie.skills}</section>
+                            <section>{founderSkills}</section>
                         </div>
                     </div>
                     <div><img src={`${meetStephanie.image}`} alt="meetStephanie-box" /></div>
