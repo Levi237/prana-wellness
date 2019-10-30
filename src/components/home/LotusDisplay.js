03eacb0ef6ab48627741176a82e9fb3f453d895b
Levi Winans <levieiko@gmail.com>
2019-10-30 14:01:52 -0700

    try new header image

--- src/components/home/LotusDisplay.js
+++ src/components/home/LotusDisplay.js
@@ -9,7 +9,7 @@ import * as routes          from '../../constants/routes';
 const lotusStyleOverride = {
     position: 'relative',
     overflow: 'hidden',
-    // maxHeight: '957px'
+    height: 'auto'
 }
 
 export default class LotusDisplay extends Component {
