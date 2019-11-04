f0eaa11307a6eb80c3df83780512f4c25773cfe0
Levi Winans <levieiko@gmail.com>
2019-11-03 20:01:18 -0800

    update admin form with timestamps

--- src/components/AdminPortal.js
+++ src/components/AdminPortal.js
@@ -44,25 +44,46 @@ export default class AdminPortal extends Component {
     render(){
         const { contacts, emailers } = this.state;
         const emailerList = emailers.map((email, key) => {
+            let time = email.timestamp.toDate().toLocaleDateString();
             return(
                 <div key={key} style={{border: '1px solid black'}}>
-                    <section>{email.email}</section>
+                    <section style={{width: '30vw', display: 'inline-block'}}>{email.email}</section><section style={{width: '9vw', display: 'inline-block'}}>{time}</section>
                 </div>
             )
         })
         const contactList = contacts.map((contact, key) => {
-        //     const addedService = contact.addServices
-            
-        //     if (contact.addServices){
-        //     let addedServices = addedService.map((service, k) => {
-        //         return(
-        //             <li key={k}> {service}</li>
-        //         )
-        //     })
+            // let addService = []
+            let time = contact.timestamp.toDate().toDateString();
+            if (contact.addServices){
+                const addedService = contact.addServices
+                let addService = addedService.map((service, k) => {
+
+
+            // let addedServices = addedService.map((service, k) => {
+                return(
+                    <li key={k}> {service}</li>
+                )
+            })
         // } 
 
             return(
-                <div key={key} style={{border: '1px solid black'}}>
+                <div key={key} style={{border: '1px solid black', margin: '0 0 1vw 0', padding: '1vw'}}>
+                    <section style={{textAlign: 'right'}}><strong>{time}</strong></section> 
+                    <section><strong>Contact Name:</strong> {contact.fromName}</section>
+                    <section><strong>Contact Email:</strong> {contact.fromEmail}</section>
+                    <section><strong>Referral Name:</strong> {contact.toName}</section>
+                    <section><strong>Referral Email:</strong> {contact.toEmail}</section>
+                    <section><strong>Business/Location:</strong> {contact.businessName}</section>
+                    <strong>Additional Services:</strong>
+                    <ul>{addService}</ul>
+                    <section><strong>Subject:</strong> {contact.subjectTitle}</section>
+                    <section><strong>Content:</strong> {contact.subjectContent}</section>
+                </div>
+            )
+        } else {
+            return(
+                <div key={key} style={{border: '1px solid black', margin: '0 0 1vw 0', padding: '1vw'}}>
+                    <section style={{textAlign: 'right'}}><strong>{time}</strong></section> 
                     <section><strong>Contact Name:</strong> {contact.fromName}</section>
                     <section><strong>Contact Email:</strong> {contact.fromEmail}</section>
                     <section><strong>Referral Name:</strong> {contact.toName}</section>
@@ -74,8 +95,9 @@ export default class AdminPortal extends Component {
                     <section><strong>Content:</strong> {contact.subjectContent}</section>
                 </div>
             )
+        }
         })
-        return(
+            return(
             <div style={{width: '100vw', position: 'relative'}}>
                 <div style={{float: 'left', width: '40vw', maxHeight: '80vh', padding: '5vw 2.5vw 5vw 7.5vw', background: '#fff', overflow: 'auto'}}>
                     {contactList}
