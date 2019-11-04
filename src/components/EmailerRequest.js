1d07dc19f5c408b498e16ee0bf3cf7dc2578df34
Levi Winans <levieiko@gmail.com>
2019-11-03 16:12:15 -0800

    reset submit afterwards

--- src/components/EmailerRequest.js
+++ src/components/EmailerRequest.js
@@ -57,25 +57,26 @@ export default class EmailerRequest extends Component {
         email: null,
     };
 
-
     handleSubmit = async (e) => {
         e.preventDefault();
-        // sgMail.send(msg);
         const newFromDB = await firebase.firestore()
             .collection('emailers')
             .add({
                 email: this.state.email,
                 timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-            })
-        return newFromDB
-        
+            }).then(
+                this.setState({
+                    email: "Thank You"
+                })
+            )
+        return newFromDB; 
     };
+
     handleChange = (e) => {
         e.preventDefault();
         this.setState({
             email: e.currentTarget.value
         });
-
     };
 
     render(){
@@ -86,7 +87,6 @@ export default class EmailerRequest extends Component {
             <>
             <form className="feedback-form" onSubmit={(e) => {this.handleSubmit(e)}}>
                 <button type="submit" style={btnStyle}><div style={arrowStyle}></div></button>
-                    {/* <button type="submit"><div></div></button> */}
                 <input
                     className="emailer-input"
                     name="email"
@@ -97,7 +97,6 @@ export default class EmailerRequest extends Component {
                     value={ email ? email : ""}
                     style={inputStyle}
                 />
-
             </form>
             </>
         );
