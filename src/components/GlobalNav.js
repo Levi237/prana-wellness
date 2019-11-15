980c11ca5786e9325b37ae22e45d9428c36d2012
Levi Winans <levieiko@gmail.com>
2019-11-15 15:15:20 -0800

    add Link to global nav logo

--- src/components/GlobalNav.js
+++ src/components/GlobalNav.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as routes from '../constants/routes'
 
  const GlobalNav = ({ toggleHamburger, logout, user }) => {
 
     return(<>
           <div className="header-logo">
-            <div className="logo-box">
-              <img src="../icon_fullcolor.png" alt="icon-fullcolor" />
-            </div>
+            <Link to={routes.HOME}>
+              <div className="logo-box">
+                <img src="../icon_fullcolor.png" alt="icon-fullcolor" />
+              </div>
+            </Link>
           </div>
 
           <div className="global-hamburger" onClick={() => toggleHamburger()}>
