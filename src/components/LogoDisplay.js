4ed4df00bac24e6f1298cd95665db8f07fbf6311
Levi Winans <levieiko@gmail.com>
2019-11-16 09:53:56 -0800

    clean up LogoDisplay component code, change from component to constant

--- src/components/LogoDisplay.js
+++ src/components/LogoDisplay.js
@@ -1,39 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import './LogoDisplay.css'
+import './LogoDisplay.css';
 
-export default class CustomerDisplay extends Component {
-    state = {
-        // logos: [
-        //     'bpr-logo-landscape-highres.png',
-        //     'centric-logo.png',
-        //     'cross-campus-high-res-logo-stacked.png',
-        //     'DocuSign_logo.png',
-        //     'KAPOW.png',
-        //     'LA-South-Chamber.png',
-        //     'salisian-lee-logo.png',
-        // ]
-    };
-    render(){
-        const { logos } = this.props;
+const CustomerDisplay = ({ logos }) => {
 
-        const logoList = logos.map((logo, key) => {
-            return(
-                <section key={key}>
-                {logo.link 
-                ?   <a href={logo.link} target="_blank" rel="noopener noreferrer">
-                        <img src={`./customer-logo/${logo.name}`} alt={logo.name}/>
-                    </a>
-                : <img src={`./customer-logo/${logo.name}`} alt={logo.name}/>
-                }
-                </section>
-            )
-        });
-
+    const logoList = logos.map((logo, key) => {
         return(
-            <div className="scrollmenu">
-                <div>{logoList}{logoList}</div>
-            </div>
+            <section key={key}>
+            {logo.link 
+            ?   <a href={logo.link} target="_blank" rel="noopener noreferrer">
+                    <img src={`./customer-logo/${logo.name}`} alt={logo.name}/>
+                </a>
+            : <img src={`./customer-logo/${logo.name}`} alt={logo.name}/>
+            }
+            </section>
         );
-    };
+    });
+
+    return(
+        <div className="scrollmenu">
+            <div>{logoList}{logoList}</div>
+        </div>
+    );
 };
+
+export default CustomerDisplay
