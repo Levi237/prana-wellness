e5c112b56b7aba171406dd7c8bfedb9a249561c8
Levi Winans <levieiko@gmail.com>
2020-03-07 15:38:28 -0800

    add video styling

--- src/components/home/HomeHeader.js
+++ src/components/home/HomeHeader.js
@@ -1,34 +1,45 @@
 import React from 'react';
 
- const HomeHeader = ({toggleContactBtn}) => {
-    return(<>
-            <video
-              playsInline
-              autoPlay
-              muted
-              loop
-              id="bgvid"
-              ref={video => {
-                  if (!video) return;
-                  video.muted = true;
-                  video.play();
-              }}
-          >
-              <source
-                  src="https://player.vimeo.com/video/395827361"
-                  type="video/mp4"
-              />
-          </video>
 
-          <div className="home-header">
-            <section className="welcomeText">WELCOME TO</section>
-            <section className="pranaText">PRANA</section>
-            <section className="wellnessText">wellness</section>
-            <section className="statementText">Wellness designed to work for you.</section>
-            <br/><br/>
-            <button name="contactForm" className="ctaBtn mobile-fill" onClick={(e) => {toggleContactBtn(e)}}>Bring Prana to Your Workplace</button>
-          </div>
-    </>);
+const HomeHeader = ({toggleContactBtn}) => {
+
+  window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+  
+  return(
+    <>
+      <div className="video-container">
+        <video
+          playsInline
+          autoPlay
+          muted
+          loop
+          id="bgvid"
+          ref={video => {
+              if (!video) return;
+              video.muted = true;
+              video.play();
+          }}
+        >
+        <source
+            src="https://player.vimeo.com/external/395830932.hd.mp4?s=d355e4cba477d5307c66b116a78ba687d56f5b7e&profile_id=174"
+            type="video/mp4"
+        />
+        </video>
+      </div>
+
+      <div className="home-header">
+        <section className="welcomeText">WELCOME TO</section>
+        <section className="pranaText">PRANA</section>
+        <section className="wellnessText">wellness</section>
+        <section className="statementText">Wellness designed to work for you.</section>
+        <button name="contactForm" className="ctaBtn mobile-fill" onClick={(e) => {toggleContactBtn(e)}}>Bring Prana to Your Workplace</button>
+      </div>
+      
+    </>)
+    ;
 };
 
 export default HomeHeader;
