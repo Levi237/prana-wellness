faad2de59d26439e1a2ebe3d2830cca2a9b483e7
Levi Winans <levieiko@gmail.com>
2020-03-11 10:05:52 -0700

    style-comp

--- src/components/LogoDisplay.js
+++ src/components/LogoDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import './LogoDisplay.css';
 
@@ -8,28 +9,54 @@ const CustomerDisplay = ({ logos, clients }) => {
     const carouselStart  = () => carouselId && carouselId.classList.add("carousel");
     const carouselTimerOff = () => carouselId && carouselId.classList.remove("carousel");
     const carouselTimerOn = () => carouselId && carouselId.classList.add("carousel");
-    const timerStart = carouselId && setTimeout(carouselStart, 1000);
-    const timerOff = carouselId && setInterval(carouselTimerOff, 50000);
-    const timerOn = carouselId && setInterval(carouselTimerOn, 100000);
+    carouselId && setTimeout(carouselStart, 1000);
+    carouselId && setInterval(carouselTimerOff, 50000);
+    carouselId && setInterval(carouselTimerOn, 100000);
 
     const logoList = logos.map((logo, key) => {
         return(
-            <section key={key}>
+            <Logo key={key}>
             {logo.link 
             ?   <a href={logo.link} target="_blank" rel="noopener noreferrer">
-                    <img src={`./customer-logo/${logo.name}`} alt={logo.name}/>
+                    <Image src={`./customer-logo/${logo.name}`} alt={logo.name}/>
                 </a>
-            : <img src={`./customer-logo/${logo.name}`} alt={logo.name}/>
+            : <Image src={`./customer-logo/${logo.name}`} alt={logo.name}/>
             }
-            </section>
+            </Logo>
         );
     });
 
     return(
-        <div id={(logos === clients) ? "scroll" : ""} className="scrollmenu">
-            <div>{logoList}</div>
-        </div>
+        <ScrollMenu id={(logos === clients) ? "scroll" : ""} className="scrollmenu">
+            <LogoList>{logoList}</LogoList>
+        </ScrollMenu>
     );
 };
 
+const ScrollMenu = styled.div`
+    background-color: rgba(150, 91, 165, 0.2);;
+    overflow: auto;
+    white-space: nowrap;
+    transition: all .3s ease-in-out;
+
+      &:hover {
+        background-color: rgba(150, 91, 165, 0.3);
+        transition: all 5s ease-in-out;
+      }
+  `;
+const LogoList = styled.div`
+    transition: all 80s ease-in-out;
+`;
+
+const Logo = styled.section`
+    height: 80px;
+    display: inline-block;
+    vertical-align: top;
+    padding: 14px;
+    margin: 10px;
+    position: relative;
+    `;
+const Image = styled.img`
+    height: 80px;
+`;
 export default CustomerDisplay
