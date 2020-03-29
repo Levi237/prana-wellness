35438aca7be0d31774400de99cc5ac483aa3527a
Levi Winans <levieiko@gmail.com>
2020-03-29 16:53:28 -0700

    clean up code

--- src/components/about/AboutMain.js
+++ src/components/about/AboutMain.js
@@ -5,15 +5,17 @@ import MeetStephanie  from './MeetStephanie';
 import TeamMembers  from './TeamMembers';
 
 const AboutMain = () => {
-    return(<Container>
-            <h1 className="main-title">a holistic approach to wellness</h1>
-            <section className="main-cta">
-              We make wellness work for you. We educate and guide you into a happier, healthier workplace. Rooted in India, at the source, our team brings experienced wellness professionals with varying backgrounds in the corporate space. Tailored programs are developed to increase employee productivity and retention, and decrease overall healthcare costs overtime. 
-            </section>
-            <MeetStephanie />
-            <TeamMembers />
-            {/* <section className="main-title-two">Meet the Team</section> */}
-    </Container>);
+    return(
+      <Container>
+        <h1 className="main-title">
+          a holistic approach to wellness
+        </h1>
+        <section className="main-cta">
+          We make wellness work for you. We educate and guide you into a happier, healthier workplace. Rooted in India, at the source, our team brings experienced wellness professionals with varying backgrounds in the corporate space. Tailored programs are developed to increase employee productivity and retention, and decrease overall healthcare costs overtime. 
+        </section>
+        <MeetStephanie />
+        <TeamMembers />
+      </Container>);
 };
 
 const Container = styled.div`
