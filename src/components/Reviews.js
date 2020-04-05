b3099fafb0c7979ff27b3a50fbb09b36d4fefe8f
Levi Winans <levieiko@gmail.com>
2020-04-05 09:21:08 -0700

    create Reviews js

---
+++ src/components/Reviews.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Reviews = () => {
+    return(<>
+            <Title>{content.header}</Title>
+
+            <section>
+                <Content>
+                    {content.message}  
+                </Content>
+                <ButtonContaner>
+                    <button className="purple mobile-fill" onClick={(e) => {toggle(e)}}>{content.button}</button>
+                </ButtonContaner>
+
+            </section>
+    </>);
+};
+
+export default Reviews;
+
+
+
+
+const Title = styled.h1 `
+    font-weight: normal;
+    color: #FFFFFF;
+    @media screen and (max-width: 945px){
+            padding: 2.5vw;
+        }
+`;
+const Content = styled.div`
+    display: inline-block;
+    vertical-align: top;
+    width: 50vw;
+    font-size: 1.5vw;
+    margin-left: 10vw;
+    margin-right: 2.5vw;
+    float: left;
+    color: #fff;
+    text-align: left;
+    @media screen and (max-width: 945px){
+        display: block;
+        width: 80vw;
+        font-size: 3vw;
+        margin-left: 10vw;
+        margin-right: 2.5vw;
+        margin-bottom: 4vw;
+        float: none;
+        text-align: justify;
+    }
+`;
+const ButtonContaner = styled.div`
+    display: inline-block;
+    vertical-align: top;
+    width: 27.5vw;
+    margin-right: 10vw;
+    float: right;
+    text-align: center;
+    @media screen and (max-width: 945px){
+
+        display: block;
+        float: none;
+        width: 100%;
+    }
+`;
