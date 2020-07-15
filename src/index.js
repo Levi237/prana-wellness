e087bf02c233ea698e1ecbaf842333bc8583d5b0
Levi Winans <levieiko@gmail.com>
2020-07-15 16:27:18 -0700

    update hosting

--- src/index.js
+++ src/index.js
@@ -5,25 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-
 import * as firebase from "firebase/app";
-// import firebase from 'firebase/app';
 import 'firebase/firestore';
-// import * as admin from 'firebase-admin';
 
 import 'firebase/firebase-storage'
-// import 'firebase/firebase-database'
-
-// const sgMail = require('@sendgrid/mail');
-// sgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
-// const msg = {
-//   to: 'test@example.com',
-//   from: 'test@example.com',
-//   subject: 'Sending with Twilio SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// };
-// sgMail.send(msg);
 
   firebase.initializeApp({
     apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
