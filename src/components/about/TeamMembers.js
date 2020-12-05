cd98b9469b0175d435993b6d2050b030a9d293d4
Levi Winans <levieiko@gmail.com>
2020-12-04 16:36:44 -0800

    add team stats

--- src/components/about/TeamMembers.js
+++ src/components/about/TeamMembers.js
@@ -5,21 +5,22 @@ import './TeamMembers.css'
 export default class TeamMembers extends Component {
     state = {
         team: [{
-            name: "Safiya",
-            skills: [
-                "300 RYT Meditation & Yoga Nidra",
-                "200RYT Yoga",
-                "Holistic Health Coach",
-                ],
-            description: 
-                "Safiya educates individuals on how to heal from anxiety and burnout while building a foundation of health to thrive through mentorship, events, and retreats. A disruptor of hustle culture, Safiya is passionate about educating people on how they can take tangible steps towards incorporating self-care and mindfulness into their daily lives. She believes that you don’t have to sacrifice health to make an impact, and that health looks different to each person."
-            ,
-            image: "safiya.png",
-        },{
+        //     name: "Safiya",
+        //     skills: [
+        //         "300 RYT Meditation & Yoga Nidra",
+        //         "200RYT Yoga",
+        //         "Holistic Health Coach",
+        //         ],
+        //     description: 
+        //         "Safiya educates individuals on how to heal from anxiety and burnout while building a foundation of health to thrive through mentorship, events, and retreats. A disruptor of hustle culture, Safiya is passionate about educating people on how they can take tangible steps towards incorporating self-care and mindfulness into their daily lives. She believes that you don’t have to sacrifice health to make an impact, and that health looks different to each person."
+        //     ,
+        //     image: "safiya.png",
+        // },{
             name: "Alexeyeva",
             skills: [
                 "200CYT Meditation",
                 "RYT200 Yoga",
+                "Reiki 1 & 2"
                 ],
             description: 
                 "Alexeyeva is an advocate, mentor, and educator for mindfulness and conscious breathing. She knows the power of the breath and how it can transform a situation - or a life. She believes that meditation is a call home to yourself and that the breath is a direct pathway to get there. Alexeyeva is passionate about people and curious about their lives. She enjoys creating a safe and comfortable space, free of judgment, where individuals can come to sit, slow down, explore and reconnect with themselves and their purpose through the power and sensations of their own breath. She believes in sharing practical mindfulness and breathing techniques that can be applied in daily life and that when someone is ready and willing to surrender to the power of their breath, it is an invitation for the ultimate journey of self-love to begin."
@@ -30,7 +31,7 @@ export default class TeamMembers extends Component {
             skills: [
                 "200Hour RYT Yoga Instructor",
                 " Certified Group Fitness Instructor",
-                "MBA Candidate C/O2021 -USC Marshall",
+                "MBA Candidate C/O2021 -USC Marshall"
                 ],
             description: 
                 "Melanie is a yoga teacher at Corepower Yoga where she's been teaching for almost 3 years. She has an incredible passion for holistic health and wellness and has integrated her love for wellness into her own career. Currently, she is an entrepreneur within the health and wellness space. Through her company's wellness devices, she not only hopes to raise awareness about the importance of preventative health but to also provide an accessible, customizable, and convenient way for people to prioritize their daily wellness."
@@ -41,7 +42,7 @@ export default class TeamMembers extends Component {
             name: "Keren",
             skills: [
                 "200RYS Yoga Instructor", 
-                "Yoga Therapist in training",
+                "Yoga Therapist in training"
                 ],
             description: 
                 "Keren’s Yoga journey began after years of struggling with her body image. She found a way to connect and love herself through Yoga. After becoming a lawyer in Israel and completing an MBA, she followed her heart and dedicated herself to the practice and study of Yoga. Keren is passionate about passing on the gift of Yoga as a tool for self-transformation and is currently on the path to becoming a Yoga Therapist. Her classes are a balance between a strong and safe physical practice and a tranquil state of mind. In her classes, she weaves in elements of philosophy, pranayama, and meditation."            ,
@@ -52,6 +53,7 @@ export default class TeamMembers extends Component {
             skills: [
                 "200 hour RYT Yoga Instructor",
                 "50 hour RYT Continued Ed",
+                "Reiki Master"
                 ],
             description: 
                 "Nadirah bases her yoga practice as the mind, body, and spirit connecting, through breath and movement. As an entertainer, she is well aware of the many benefits yoga and meditation has to offer in real life situations. The Chicago native wholeheartedly believes that yoga is a journey to everlasting inner peace and oneness with self. This in turn, teaches us to self-love through listening, learning, and laughing, in order to shine our light and inspire others to do the same.",
