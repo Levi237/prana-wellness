810ff0cb128ce61d5d54726a5b26852b4575abbc
Levi Winans <levieiko@gmail.com>
2020-12-06 18:48:37 -0800

    update firebase

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 import 'firebase/firestore';
 
 import 'firebase/firebase-storage'
