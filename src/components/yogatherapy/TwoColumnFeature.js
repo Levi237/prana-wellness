e857cf6c5993cffeace3fc43db29fbd518fa5245
Levi Winans <levieiko@gmail.com>
2020-12-07 20:15:56 -0800

    create new component

---
+++ src/components/yogatherapy/TwoColumnFeature.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styled               from 'styled-components';
+
+export default class TwoColumnFeature extends Component {
+    state = {
+
+    };
+
+    render(){
+
+        return(
+            <>
+
+            </>
+        );
+    };
+};
