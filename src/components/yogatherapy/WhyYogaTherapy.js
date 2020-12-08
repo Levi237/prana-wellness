f8f51a3efdbfcd975d51fec5b2b1f6caeccdbfb5
Levi Winans <levieiko@gmail.com>
2020-12-07 20:29:31 -0800

    update image

--- src/components/yogatherapy/WhyYogaTherapy.js
+++ src/components/yogatherapy/WhyYogaTherapy.js
@@ -8,7 +8,7 @@ export default class WhyYogaTherapy extends Component {
             title: "Why Yoga Therapy?",
             subtitle: "WHAT I SAY TO THOSE WHO HAVEN'T TRIED IT",
             content: "Most people are unaware of the power they possess within their own body to heal itself. Yoga Therapy is a personal, tailored approach to healing both mental and physical challenges. It is the perfect marriage of Eastern and Western medicine, and an alternative to traditional methods. Every session with me, you will feel safe and free to exist in your own body and in your personal truth. Yoga Therapy is specific and focused, using traditional practices to address health challenges, reduce symptoms, restore balance, manage a condition, increase vitality, and improve mental health.",
-            image: "about-meet.png",
+            image: "yoga-therapy.jpg",
             servicesTitle: "HOW CAN WE WORK TOGETHER?",
             services: [
                 'Private one on one Yoga Therapy',
