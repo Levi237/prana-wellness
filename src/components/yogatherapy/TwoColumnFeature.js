fb829dab422f3bcf091238105489f5a046a30db4
Levi Winans <levieiko@gmail.com>
2020-12-08 11:18:45 -0800

    add copy to state and hardcode content

--- src/components/yogatherapy/TwoColumnFeature.js
+++ src/components/yogatherapy/TwoColumnFeature.js
@@ -3,37 +3,83 @@ import styled               from 'styled-components';
 
 export default class TwoColumnFeature extends Component {
     state = {
-        stats: {
+        leftInfo: {
+            title: 'COMMON ISSUES TREATED',
             info1: [
-                "Chronic pain",
-                "Arthritis",
-                "Hips",
-                "Knees",
-                "Shoulder",
-                "Lower back",
+                'Chronic pain',
+                'Arthritis',
+                'Hips',
+                'Knees',
+                'Shoulder',
+                'Lower back',
             ],
             info2: [
-                "Depression",
-                "Anxiety",
-                "Obesity",
-                "Hypertension",
-                "Accident recovery",
-                "Post-surgery rehab",
+                'Depression',
+                'Anxiety',
+                'Obesity',
+                'Hypertension',
+                'Accident recovery',
+                'Post-surgery rehab',
             ]
+        },
+        rightInfo: {
+            title: 'CUSTOMIZED THERAPY',
+            info1: [
+                'Thorough physical evaluation',
+                'Custom "yoga prescription"',
+                'Complimentary consult',
+                'Weekly sessions',
+                'Virtual and in-person',
+                'Specialist referral network:',
+                '(chiropractic, MD, physical therapy)'          
+            ],
         }
     };
 
     render(){
-
+        const { leftInfo, rightInfo } = this.state
         return(
             <Container>
                 <LeftColumn>
-                    <img src="guru.png"/>
-                    <div>TEST</div>
+                    <img src="bamboo.jpg"/>
+                    <div>
+                        <h1>
+                            {leftInfo.title}
+                        </h1>
+                        <ul>
+                            <li>{leftInfo.info1[0]}</li>
+                            <li>{leftInfo.info1[1]}</li>
+                            <li>{leftInfo.info1[2]}</li>
+                            <li>{leftInfo.info1[3]}</li>
+                            <li>{leftInfo.info1[4]}</li>
+                            <li>{leftInfo.info1[5]}</li>
+                        </ul>
+                        <ul>
+                            <li>{leftInfo.info2[0]}</li>
+                            <li>{leftInfo.info2[1]}</li>
+                            <li>{leftInfo.info2[2]}</li>
+                            <li>{leftInfo.info2[3]}</li>
+                            <li>{leftInfo.info2[4]}</li>
+                            <li>{leftInfo.info2[5]}</li>
+                        </ul>
+                    </div>
                 </LeftColumn>
                 <RightColumn>
-                    <img src="bamboo.jpg"/>
-                    <div>TEST</div>    
+                    <img src="guru.png"/>
+                    <div>                        
+                        <h1>
+                            {rightInfo.title}
+                        </h1>
+                        <ul>
+                            <li>{rightInfo.info1[0]}</li>
+                            <li>{rightInfo.info1[1]}</li>
+                            <li>{rightInfo.info1[2]}</li>
+                            <li>{rightInfo.info1[3]}</li>
+                            <li>{rightInfo.info1[4]}</li>
+                            <li>{rightInfo.info1[5]}</li>
+                            <li>{rightInfo.info1[6]}</li>
+                        </ul>
+                    </div>    
                 </RightColumn>
             </Container>
         );
@@ -47,37 +93,66 @@ const Column = styled.div`
     display: inline-block;
     font-size: 0;
     width: 50%;
+    vertical-align: top;
     div {
         display: inline-block;
         font-size: 3vw;
         color: white;
         height: 25vw;
-        width: 33.33vw;
+        width: 35vw;
         margin: 5vw 2.5vw;
         text-align: center;
         background-color: #fff;
+        color: black;
     }
     img {
-        margin: 10vw 6vw;
+        position: absolute;
+        margin: 9vw 3.5vw;
         width: 15vw;
         border-radius: 100%;
-        border: 5px solid #C09EC8;
-        position: absolute;
+        border: 1vw solid #C09EC8;
+        background-color: #C09EC8;
+    }
+    h1 {
+        font-size: 1.8vw;
+        color: green;
+        margin-block-end: 1vw!important;
+    }
+    ul {
+        display: inline-block;
+        font-size: 1.25vw;
+        color: purple;
+        text-align: left;
+        line-height: 2.25vw;
+        margin-block-start: 1vw;
+        margin-block-end: 1vw;
+        padding-inline-start: 3vw;
     }
 `;
 const LeftColumn = styled(Column)`
         div {
-            margin-left: 14.42vw;
+            margin-left: 12.5vw;
         }
         img {
-
+            left: 0;
+        }
+        ul {
+        }
+        ul:first-of-type {
+            margin-left: 5vw;
         }
 `;
 const RightColumn = styled(Column)`
         div {
-            margin-right: 14.42vw;
+            margin-right: 12.5vw;
         }
         img {
-right: 0;
+            right: 0;
+        }
+        ul {
+            margin-right: 5vw;
+        }
+        li:last-of-type {
+            list-style-type: none;
         }
 `;
