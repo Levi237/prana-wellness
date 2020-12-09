74dbee06a0504496841fa00cfdd2f818cd604527
Levi Winans <levieiko@gmail.com>
2020-12-09 00:53:34 -0800

    add boxes to circle feature

--- src/components/prenatal/PrenatalMain.js
+++ src/components/prenatal/PrenatalMain.js
@@ -4,12 +4,19 @@ import styled from 'styled-components';
 const PrenatalMain = () => {
     return(
       <Container>
+        <div>
         <h1 className="main-title">
         PUT A TITLE HERE
         </h1>
         <section className="main-cta">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </section>
+        </div>
+        <Circle>
+          <div></div>
+          <section></section>
+          <section></section>
+        </Circle>
       </Container>);
 };
 
@@ -24,10 +31,36 @@ const Container = styled.div`
       margin-left: 2vw;
       margin-right: 2vw;
     }
+  > div {
+    background-color: white;
+    position: relative;
+    z-index: 9999999999999;
+  }
     @media screen and (max-width: 945px) {
       margin-top: calc(32.3vw - 100vh + 58px);
       padding-top: 5vw;
     }
 `;
-
+const Circle = styled.section`
+  position: relative;
+  height: 75vw;
+  > div {
+    background-color: #965BA5;
+    width: 50vw;
+    height: 50vw;
+    border-radius: 100%;
+    margin: -20vw 25vw;
+    position: absolute;
+    opacity: .5;
+  }
+  > section {
+    display: inline-block;
+    width: 50vw;
+    height: 50vw;
+    margin-top: 7vw;
+  }
+  > section:last-of-type {
+    background-color: purple;
+  }
+`;
 export default PrenatalMain;
