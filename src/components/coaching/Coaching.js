d35e7c28271b398f6041e3839233250b285562e2
Levi Winans <levieiko@gmail.com>
2020-12-12 15:02:32 -0800

    arrow box for coaching

--- src/components/coaching/Coaching.js
+++ src/components/coaching/Coaching.js
@@ -11,7 +11,7 @@ export default class Coaching extends Component {
             "Prove client ROI",
             "Motivate your team ",
             "Identify ideal clients ",
-            "Identify & overcome challenges"
+            "Resolve challenges"
         ]
     };
 
@@ -31,51 +31,88 @@ export default class Coaching extends Component {
             <Container>
                 <h1>GROWING YOUR BUSINESS</h1>
                 <h3>Coaching Your Leadership Team to Greatness</h3>
+                <section></section>
+                <br/><br/>
                 {list}
             </Container>
         );
     };
 };
 
+const ArrowBackgroundColor = "rgba(150, 91, 165, 1)"
+const ArrowColor = "#fff"
 const Container = styled.div`
+    width: 650px;
+    margin: 100px auto;
     text-align: center;
+    border: 0 solid black;
+    padding: 0 0 40px 0;
+    border-radius: 15px;
+    overflow: hidden;
+    box-shadow: 2px 2px 70px rgba(0,0,0,.1);
     > h1 {
-        margin-top: 40px;
+        width: 100%;
+        padding: 40px 0 5px;
+        font-weight: 300;
+        color: ${ArrowColor};
+        background-color: ${ArrowBackgroundColor};
     }
     > h3 {
+        width: 100%;
+        margin: 0 auto;
         font-family: Merriweather;
-        font-size: 24px;
+        font-size: 23px;
+        font-weight: 100;
+        color: ${ArrowColor};
+        padding-bottom: 10px;
+        background-color: ${ArrowBackgroundColor};
+    }
+    > section {
+        border-bottom: none;
+        border-top: 50px solid ${ArrowBackgroundColor};
+        border-left: 325px solid transparent;
+        border-right: 325px solid transparent;
     }
     > div {
         font-size: 18px;
-        width: 320px;
+        width: 220px;
         margin: 0 auto;
         text-align: left;
         padding: 10px;
-        
+        display: inline-block;
         img {
             width: 20px;
             padding-right: 10px;
         }
     }
-    @media screen and (max-width: 945px) {
+    @media screen and (max-width: 800px) {
+        width: 90vw;
+        margin: 100px auto;
+        text-align: center;
+        border: 0 solid black;
+        padding: 0 0 40px 0;
+        border-radius: 15px;
+        overflow: hidden;
+        box-shadow: 2px 2px 70px rgba(0,0,0,.15);
         > h1 {
-            margin-top: 15vw;
+            width: 100%;
+            padding: 8vw 0 2vw;
+            font-size: 8vw;
         }
         > h3 {
-            font-family: Merriweather;
-            font-size: 7vw;
+            width: 80%;
+            margin: 0 auto;
+            font-size: 5vw;
+            padding: 0 10%;
+        }
+        > section {
+            border-top: 10vw solid ${ArrowBackgroundColor};
+            border-left: 45vw solid transparent;
+            border-right: 45vw solid transparent;
         }
         > div {
-                font-size: 4vw;
-                width: 70vw;
-                margin: 0 auto;
-                text-align: left;
-                padding: 10px 0;
             img {
-                width: 3vw;
-                padding: 0 2vw;
             }
         }
-    }
+    }    
 `;
