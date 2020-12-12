a62a6469181289c395640e41c64e48e71dd76db7
Levi Winans <levieiko@gmail.com>
2020-12-12 03:05:18 -0800

    styling updates

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,68 +3,81 @@ import styled from 'styled-components';
 
  const HeaderComponent = ({title, subtitle, image, purpleBox}) => {
     return(<>
-    <Container className="header-container">
-        <Image className="header-image" src={image} alt="wellness-header"/>
-        <Content className="header-content-container">
-            <section className="top-header-text pranaText">{title}</section>
-            <section className="bottom-header-text">{subtitle}</section>
-        </Content>
+    <Container style={{ backgroundImage: `url(${image})` }}>
+        <div>
+            <div>
+                <section>{title}</section>
+                <section>{subtitle}</section>
+            </div>
+        </div>
     </Container>
         {purpleBox && <PurpleBox className="wellness-header-purple"></PurpleBox>}
     </>);
 };
 const PurpleBox = styled.div`
-position: relative;
+    position: relative;
     z-index: 0;
     margin-top: -15vw;
     height: 30vw;
     background-color: #bf9ec8;
 `;
 const Container = styled.div`
-color: white;
-// padding-top: 120px;
-background-color: transparent;
-
-@media screen and (max-width: 945px) {
-      height: 44vw;
-      text-align: center;
-      overflow: hidden;
+    position: relative;
+    color: #fff;
+    width: 80vw;
+    height: 27vw;
+    margin: 0 10vw;
+    z-index: 11;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    > div {
+        z-index: 12;
+        width: 80vw;
+        text-align: center;
+        > div {
+            section {
+                &:first-of-type {
+                    text-align: center;
+                    letter-spacing: .4vw;
+                    color: #fff !important;
+                    font-size: 3.5vw;
+                    font-weight: light;
+                    padding-top: 8vw;
+                }
+                &:last-of-type {
+                    font-family: 'Merriweather', sans-serif;
+                    font-style: italic;
+                    font-size: 7vw;
+                  
+                    text-align: center;
+                    letter-spacing: .3vw;
+                    color: #FFF;
+                }
+            }
+        }
+    }
+    @media screen and (max-width: 945px) {
+        width: 100vw;
+        margin: 0;
+        height: 30vw;
+        text-align: center;
+        overflow: hidden;
         padding-top: 0;
+        > div > div {
+            width: 100vw;
+            section:first-of-type {
+                font-size: 5vw;
+            }
+            section:last-of-type {
+                font-size: 9vw;
+            }
+        }
     }
 `;
-const Image = styled.img`
-width: 80vw;
-margin: 0 10vw;
-position: relative;
-z-index: 1;
 
-@media screen and (max-width: 945px) {
-width: 100vw;
-margin: 0;
-z-index: 1;
-height: auto;
-position: relative;
-}
-`;
 
-const Content = styled.div`
-position: absolute;
-z-index: 11;
-text-align: center;
-width: 80vw;
-margin: -20vw 10vw;
 
-@media screen and (max-width: 945px) {
-    width: 100vw;
-    margin: -25vw 0;
-    section:first-of-type {
-        font-size: 5vw;
-      }
-      section:last-of-type {
-        font-size: 9vw;
-      }
-}
-`;
 
 
 export default HeaderComponent;
