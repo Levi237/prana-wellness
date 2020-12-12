49b0180c0c3c48631b1c73afb27de899ded12608
Levi Winans <levieiko@gmail.com>
2020-12-11 16:04:28 -0800

    style container

---
+++ src/components/coaching/CoachingMain.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import FeatureFour from './FeatureFour';
+
+const CoachingMain = () => {
+    return(
+      <Container>
+        <h1 className="main-title">
+        SPEAKING TO INSPIRE
+        </h1>
+        <section className="main-cta">
+        Book Stephanie, “Motivator in Chief,” to motivate, inspire, and provide valuable tools to educate your team at work, youth group, newbie entrepreneurs, or wellness providers.
+        </section>
+      <FeatureFour/>
+    </Container>);
+};
+
+const Container = styled.div`
+  margin-bottom: 5vw;
+  margin-top: calc(25.86vw - 100vh + 196px);
+  position: relative;
+  z-index: 1;
+  padding-bottom: 8vw;
+
+  button {
+      margin-left: 2vw;
+      margin-right: 2vw;
+    }
+    @media screen and (max-width: 945px) {
+      margin-top: calc(32.3vw - 100vh + 58px);
+      padding-top: 5vw;
+    }
+`;
+export default CoachingMain;
