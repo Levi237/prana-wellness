7b7097519f8a2b5b6b70dc43133a31d433ea6ca5
Levi Winans <levieiko@gmail.com>
2020-12-27 21:14:00 -0800

    add email confirmation pate

--- src/components/contact/EmailConfirmation.js
+++ src/components/contact/EmailConfirmation.js
@@ -5,11 +5,20 @@ export default class EmailConfirmation extends Component {
     state = {}
     render(){
         const { toggleEmailConfirmation } = this.props
-        // setTimeout(function(){history.back();}, 3000);
         return (<>
             <Container id="confirmation" className="active contact-container">
                 <CloseBtn name="emailForm" className="close xClose white" onClick={(e) => {toggleEmailConfirmation(e)}}>X</CloseBtn>
-                Thank you for your Email Submission.  Plesae check your inbox for your confirmaton email soon.
+                <div>
+                    <div>
+                        <section>
+                            Namaste!
+                            <br/>
+                            <p>
+                                Thank you for your Email Submission.  Please check your inbox for your confirmaton email soon.
+                            </p>
+                        </section>
+                    </div>
+                </div>
             </Container>
         </>);
     };
@@ -18,6 +27,49 @@ const Container = styled.div`
     position: fixed;
     overflow: scroll;
     z-index: 99999;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    >div {
+        display: inline-block;
+        min-height: 400px;
+        min-width: 400px;
+        max-height: 400px;
+        max-width: 400px;
+        background-image: url(floral-background.png);
+
+        > div {
+            height: 298px;
+            width: 298px;
+            margin: 50px;
+            border: 1px solid #965BA5;
+            background-color: rgba(255,255,255,1);
+            text-align: center;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            section {
+                font-family: Merriweather;
+                text-transform: capitalize;
+                font-size: 40px;
+                color: #965BA5;
+                width: 100%;
+                padding-top: 5%;
+                p {
+                    display: inline-block;
+                    padding: 5%;
+                    font-family: Montserrat;
+                    font-size: 14px;
+                    color: black;
+                }
+            }
+
+        }
+    }
 `;
 
 const CloseBtn = styled.button`
